cfe24802bd8178814891a5c7b00c1e2f1a13d7ca
mamathadharavath35-a11y <mamathadharavath35@gmail.com>
2026-07-17 21:41:58 +0530

    Update akky.js

--- akky.js
+++ akky.js
@@ -7,9 +7,10 @@ const revealElements = document.querySelectorAll(
 );
 
 const observer = new IntersectionObserver((entries) => {
+
   entries.forEach((entry) => {
 
-    if (entry.isIntersecting) {
+    if(entry.isIntersecting){
 
       entry.target.style.opacity = "1";
       entry.target.style.transform = "translateY(0)";
@@ -17,9 +18,11 @@ const observer = new IntersectionObserver((entries) => {
     }
 
   });
+
 },{
   threshold:0.15
 });
+
 
 revealElements.forEach((el)=>{
 
@@ -38,9 +41,12 @@ revealElements.forEach((el)=>{
 // LETTER TYPEWRITER
 // ==============================
 
-const letter=document.querySelector(".letter-content p");
+const letter = document.querySelector(".letter-content p");
 
-const originalText=letter.innerHTML;
+
+if(letter){
+
+const originalText = letter.innerHTML;
 
 letter.innerHTML="";
 
@@ -48,9 +54,9 @@ let i=0;
 
 function typeLetter(){
 
-  if(i<originalText.length){
+  if(i < originalText.length){
 
-    letter.innerHTML+=originalText.charAt(i);
+    letter.innerHTML += originalText.charAt(i);
 
     i++;
 
@@ -60,60 +66,51 @@ function typeLetter(){
 
 }
 
+
 window.addEventListener("load",()=>{
 
   setTimeout(typeLetter,1000);
 
 });
 
+}
 
+
+
+
+// ==============================
+// CINEMATIC ENDING
 // ==============================
-// THANK YOU FADE
-// ==============================
 
-const thank=document.querySelector(".thank-you");
-
-window.addEventListener("scroll",()=>{
-
-  const position=thank.getBoundingClientRect().top;
-
-  const screen=window.innerHeight;
-
-  if(position<screen-100){
-
-    thank.style.transition="1.2s";
-
-    thank.style.opacity="1";
-
-  }
-
-});
-
-thank.style.opacity="0";
-
-
-
-const ending = document.querySelector(".thank-you");
+const thankSection = document.querySelector(".thank-you");
 const blackScreen = document.querySelector(".black-screen");
 
 let endingPlayed = false;
 
-window.addEventListener("scroll", () => {
 
-    if (endingPlayed) return;
+window.addEventListener("scroll",()=>{
 
-    const position = ending.getBoundingClientRect().top;
 
-    if (position < window.innerHeight / 2) {
+    if(endingPlayed) return;
+
+
+    const position = thankSection.getBoundingClientRect().top;
+
+
+    if(position < window.innerHeight * 0.3){
+
 
         endingPlayed = true;
 
-        setTimeout(() => {
+
+        setTimeout(()=>{
 
             blackScreen.classList.add("show");
 
-        }, 1500);
+        },1500);
+
 
     }
+
 
 });
